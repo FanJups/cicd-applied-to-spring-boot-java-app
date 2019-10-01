315d2987929240a928f834c30e1279337984d75e
FanJups <jupsfan@gmail.com>
2019-10-01 20:40:46 +0200

    First Upload

---
+++ src/test/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cicd.cicdappliedtospringbootjavaapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdAppliedToSpringBootJavaAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
