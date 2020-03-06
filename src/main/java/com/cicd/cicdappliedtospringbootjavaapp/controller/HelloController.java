6c9cab3da580ad1ba937e410febc8ba18caab289
sudipto92 <sudiptodasec@gmail.com>
2020-03-06 10:39:04 +0530

    Create HelloController.java

---
+++ src/main/java/com/cicd/cicdappliedtospringbootjavaapp/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.cicd.cicdappliedtospringbootjavaapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+        @GetMapping("/")
+        public String home() {
+        return "Hello World from DZONE";
+    }
+        @GetMapping("/sudo ")
+        public String home1() {
+        return "Spring Boot";
+    }
+}
