ce36dd295f91bfed454b1204a5744ea8dcdbe71c
sudipto92 <sudiptodasec@gmail.com>
2020-07-15 16:26:42 +0530

    Update deploy.sh

--- ansiblek8/roles/deploy/files/deploy.sh
+++ ansiblek8/roles/deploy/files/deploy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-sudo scp /etc/kubernetes/admin.conf $HOME/
-sudo chown $(id -u):$(id -g) $HOME/admin.conf
-export KUBECONFIG=$HOME/admin.conf
-kubectl delete pod app-pod
-kubectl apply -f /home/k8s-admin/k8/app-pod.yml
+#sudo scp /etc/kubernetes/admin.conf $HOME/
+#sudo chown $(id -u):$(id -g) $HOME/admin.conf
+#export KUBECONFIG=$HOME/admin.conf
+
+
+kubectl get services | grep knote
+RESULT=$?
+		
+if [ $RESULT -eq 0 ]; then
+		kubectl delete -f /tmp/deployment/deploy.yml
+    kubectl apply -f /tmp/deployment/deploy.yml
+	else
+		kubectl delete -f /tmp/deployment
+		kubectl apply -f /tmp/deployment/
+fi
+
+
+
+
